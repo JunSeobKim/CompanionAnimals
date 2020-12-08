4c909c59c758c4f1ed27710405b16e820ee3aa82
JunSeobKim <wnstjq616@gmail.com>
2020-12-08 18:41:23 +0900

    Edit storyboard

---
+++ CompanionAnimals/DescriptionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DescriptionViewController.swift
+//  CompanionAnimals
+//
+//  Created by 김준섭 on 2020/12/08.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
